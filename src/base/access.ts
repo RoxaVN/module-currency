f368acdffaa9be6c6bc5bbb672fb08bed0a4641d
woody <woodpecker146@gmail.com>
2023-08-24 09:17:04 +0700

    feat: init module

---
+++ src/base/access.ts
@@ -0,0 +1,18 @@
+import { accessManager } from '@roxavn/core/base';
+
+import { baseModule } from './module.js';
+
+export const scopes = accessManager.makeScopes(baseModule, {
+  CurrencyAccount: { name: 'currencyAccount' },
+  Transaction: { name: 'transaction' },
+});
+
+export const permissions = accessManager.makePermissions(scopes, {
+  ReadAccountTransactions: {
+    allowedScopes: [
+      accessManager.scopes.ResourceOwner(scopes.CurrencyAccount.name),
+    ],
+  },
+});
+
+export const roles = accessManager.makeRoles(scopes, permissions, {});
