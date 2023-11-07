7e948b0d00099baa6d6f5c6eef60bc59b0eb0298
woody <woodpecker146@gmail.com>
2023-11-07 17:27:10 +0700

    fix: not export transaction

--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -2,4 +2,4 @@ export * from './currency.js';
 export * from './currency.rate.js';
 export * from './currency.account.js';
 export * from './account.transaction.js';
-export * from './currency.account.js';
+export * from './transaction.js';
